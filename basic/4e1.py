60bf088826ad327c7e473965f25c2473c17794ce
Minso Jeong <minso.jeong@daum.net>
2021-05-01 03:28:22 +0900

    Update 4e1.py
    Optimized

--- basic/4e1.py
+++ basic/4e1.py
@@ -8,12 +8,16 @@
 n = int(input())
 D = input().split()
 
+x = 1
+y = 1
+
+# version 1
+'''
 D_list = ['L', 'R', 'U', 'D']
 dx = [0, 0, -1 ,1]
 dy = [-1, 1, 0, 0]
 
-x = 1
-y = 1
+
 for d in D:
     for i in range(4):
         if d == D_list[i]:
@@ -24,4 +28,24 @@ for d in D:
         continue
     x = nx
     y = ny
+'''
+
+# version 2
+D_list = {'L': 0, 'R' : 1, 'U': 2, 'D': 3}
+dx = [0, 0, -1 ,1]
+dy = [-1, 1, 0, 0]
+
+for d in D:
+    if d in D_list:
+        nx = x +dx[D_list[d]]
+        ny = y +dy[D_list[d]]
+    else:
+        continue
+    if nx <1 or ny <1 or nx>n or ny>n:
+        continue
+    x = nx
+    y = ny
+
 print(x, y)
+
+
